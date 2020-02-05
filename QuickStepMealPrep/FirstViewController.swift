9860d9cf5d4ce9bed1783aa731bb15583ebb9fc5
Jon <jdcorn@live.com>
2020-02-05 12:31:08 -0700

    initial commit

---
+++ QuickStepMealPrep/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  QuickStepMealPrep
+//
+//  Created by Jon Corn on 2/5/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QuickStepMealPrep/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  QuickStepMealPrep
+//
+//  Created by Jon Corn on 2/5/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
